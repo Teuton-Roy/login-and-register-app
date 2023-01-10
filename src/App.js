e6bf4f01907e3dd819e8e6e6cf7331e3d258347c
Teuton Roy <popeyehk404@gmaill.com>
2023-01-10 18:47:59 +0530

    install (npm i react-router-dom) for routing every page and code change in App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,39 @@ import './App.css'
 import Homepage from "./components/homepage/homepage"
 import Login from "./components/login/login"
 import Register from "./components/register/register"
+//import react-dom//
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Homepage /> */}
+
+      <Router>
+        <Switch>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/register">
+            <Register />
+          </Route>
+
+          <Route path="/">
+            <Homepage />
+          </Route>
+
+        </Switch>
+      </Router>
+
+      {/* <Homepage />
       <Login />
-      {/* <Register /> */}
+      <Register /> */}
     </div>
   );
 }
