411bb181f6406cafa5e5cffac66c842c0f66724e
Teuton Roy <popeyehk404@gmaill.com>
2023-01-10 19:33:53 +0530

    Add a <marquee/> tag in homepage and create a link

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -3,12 +3,18 @@ import "./homepage.css"
 
 const Homepage = () => {
     return (
-        <div className="homepage">
-            <h2>Hello World</h2>
-            <div className="button">
-                Logout
+        <div className="mar">
+            <marquee direction="lr"><a href="https://www.nitjsr.ac.in">Wellcome to NIT Jamshedpur</a></marquee>
+
+            <div className="homepage">
+                <h2>Wellcome to your account</h2>
+                <div className="button">
+                    Logout
+                </div>
             </div>
         </div>
+        // <marquee behavior="alternate"direction="ltr">copyright@ Teuton Roy</marquee>
+        // <marquee>tjnjng</marquee>
     )
 }
 
